0aa68d7fa698cced1e8e92f6438e20a741f708b6
Ruslan1908 <92510484+Ruslan1908@users.noreply.github.com>
2024-10-25 16:39:22 +0200

    Add files via upload
    Added eslint configurations

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser
+    },
+    settings: {
+      react: {
+        version: "detect"  
+      }
+    },
+    plugins: {
+      react: pluginReact
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
