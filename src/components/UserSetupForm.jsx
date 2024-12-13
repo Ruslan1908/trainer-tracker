a487fc7dee5d406b86ffd231af2c000e14f6e6ba
Ruslan1908 <92510484+Ruslan1908@users.noreply.github.com>
2024-12-13 22:55:45 +0100

    Update UserSetupForm.jsx

--- src/components/UserSetupForm.jsx
+++ src/components/UserSetupForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { TextField, Button, MenuItem } from '@mui/material';
 
-import { saveUserData } from './firebaseService';
+import { saveUserData } from './services/firebaseService';
 
 const initialPhysicalParams = {
   weight: localStorage.getItem('weight') || '',
