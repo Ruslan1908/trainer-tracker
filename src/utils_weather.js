8eb15625db5a1c19d51aa07bfa0532d8e2167d68
Ruslan1908 <92510484+Ruslan1908@users.noreply.github.com>
2024-12-13 22:36:50 +0100

    Update utils_weather.js
    теперь город

--- src/utils_weather.js
+++ src/utils_weather.js
@@ -2,13 +2,19 @@ import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY;
 
-// Функция для запроса погоды по координатам
-export const fetchWeatherData = async (lat, lon) => {
+// Функция для запроса погоды 
+export const fetchWeatherData = async (city) => {
   try {
     const response = await axios.get(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     );
-    return response.data;
+    return {
+      current: {
+        temp: response.data.main.temp,
+        humidity: response.data.main.humidity,
+        weather: response.data.weather,
+      },
+    };
   } catch (error) {
     throw new Error('Failed to fetch weather data');
   }
