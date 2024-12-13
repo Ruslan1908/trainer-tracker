d9dbbda38c545e37ec2546a55c9f84e5789f6538
Ruslan1908 <92510484+Ruslan1908@users.noreply.github.com>
2024-12-13 22:32:03 +0100

    Update App.jsx
    используется headerComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { styled } from 'styled-components';
 
-import { Header } from './Header';
+import { Header } from './HeaderComponent';
 import { UserSetupForm } from './UserSetupForm';
 import { WorkoutScheduler } from './WorkoutScheduler';
 import { Weather } from './Weather';
